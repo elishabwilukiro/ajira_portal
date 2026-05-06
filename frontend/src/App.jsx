38814c38eb205fe0ad028e264fe8446d6bb090f9
elishabwilukiro <bwilukiroelisha@gmail.com>
2026-05-06 12:52:44 +0300

    Home page created

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import reactLogo from './assets/react.svg'
+// import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Navbar from './components/Navbar'
+
+function App() {
+
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </>
+  )
+}
+
+export default App
